7eb3c26923b5b19982c0f37562f2d556e506232a
Heladio Francisco Morales Dorantes <inghfrancisco.morales@gmail.com>
2022-10-29 19:17:29 -0500

    update

--- f4cron.php
+++ f4cron.php
@@ -1,11 +1,21 @@
 <?php
 
-include("connection.php")
+include "f1conn.php";
 if(!$conn)
 {
     die("Conexion fallida" . mysqli_connect_error());
 }else{
-    echo   "Conexion exitosa frank";
+    $Mtoken = "af599b21642927b27963b1d66694896a";
+    $sql = "SELECT * FROM `datos` WHERE `token` = '$Mtoken' ";
+    $stmt = mysqli_query($conn,$sql);
+    if($stmt)
+    {
+        $jsonDataOutput=array("token"=>$Mtoken);
+        $jsonOutput=array("resconseCode"=>105,"message"=>"succes","data"=>$jsonDataOutput);
+        echo   json_encode($jsonOutput);
+    }else{
+        echo   "data not found";
+    }
 }
-mysqli_close($conn);
+
 ?>
